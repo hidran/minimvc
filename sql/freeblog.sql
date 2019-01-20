bfe37a87cbadc1573c0e80eb54044ecd81b597ae
Hidran Arias <hidran@gmail.com>
2019-01-20 21:33:36 +0100

    adds users table

--- sql/freeblog.sql
+++ sql/freeblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 20, 2019 alle 16:08
+-- Creato il: Gen 20, 2019 alle 21:31
 -- Versione del server: 5.7.19
 -- Versione PHP: 7.2.12
 
@@ -29,19 +29,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `posts` (
-  `id` int(10)  NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL,
+  `user_id` int(12) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `message` text COLLATE utf8_unicode_ci NOT NULL,
   `datecreated` datetime NOT NULL,
-  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-   PRIMARY KEY (`id`),
-   KEY `idx_title` (`title`)
+  `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dump dei dati per la tabella `posts`
---
-
 
 -- --------------------------------------------------------
 
@@ -50,26 +44,91 @@ CREATE TABLE `posts` (
 --
 
 CREATE TABLE `postscomments` (
-  `id` int(12)  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `id` int(12) NOT NULL,
   `post_id` int(10) NOT NULL,
   `comment` text COLLATE utf8_unicode_ci NOT NULL,
   `datecreated` datetime NOT NULL,
   `email` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  KEY `idx_post_id` (`post_id`)
+  `user_id` int(12) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Struttura della tabella `users`
+--
 
+CREATE TABLE `users` (
+  `id` int(12) UNSIGNED NOT NULL,
+  `username` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `fiscalcode` char(16) COLLATE utf8_unicode_ci NOT NULL,
+  `age` smallint(3) UNSIGNED NOT NULL,
+  `avatar` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `roletype` enum('admin','editor','user') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_title` (`title`),
+  ADD KEY `idx_user_id` (`user_id`);
+
+--
+-- Indici per le tabelle `postscomments`
+--
+ALTER TABLE `postscomments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_post_id` (`post_id`),
+  ADD KEY `idx_user_id` (`user_id`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `u_fiscalcode` (`fiscalcode`),
+  ADD KEY `i_email` (`email`),
+  ADD KEY `i_username` (`username`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
 
 --
 -- AUTO_INCREMENT per la tabella `posts`
 --
+ALTER TABLE `posts`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT per la tabella `postscomments`
+--
+ALTER TABLE `postscomments`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- Limiti per le tabelle scaricate
 --
+
+--
+-- Limiti per la tabella `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `f_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Limiti per la tabella `postscomments`
