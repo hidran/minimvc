1f0e65b69318c9851ca1a7073fc35407ee32e99b
Hidran Arias <hidran@gmail.com>
2019-01-22 23:44:44 +0100

    adds mysql file with nullable fields for users table

--- sql/freeblog.sql
+++ sql/freeblog.sql
@@ -1,16 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Gen 22, 2019 alle 23:43
+-- Versione del server: 5.7.19
+-- Versione PHP: 7.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `freeblog`
+--
 CREATE DATABASE IF NOT EXISTS `freeblog` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `freeblog`;
-DROP TABLE IF EXISTS `postscomments`;
-DROP TABLE IF EXISTS `posts`;
-DROP TABLE IF EXISTS `users`;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `posts`
+--
+
 CREATE TABLE `posts` (
   `id` int(10) NOT NULL,
   `user_id` int(12) UNSIGNED NOT NULL,
@@ -19,7 +38,11 @@ CREATE TABLE `posts` (
   `datecreated` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Struttura della tabella `postscomments`
+--
 
 CREATE TABLE `postscomments` (
   `id` int(12) NOT NULL,
@@ -30,51 +53,90 @@ CREATE TABLE `postscomments` (
   `user_id` int(12) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
 
 CREATE TABLE `users` (
   `id` int(12) UNSIGNED NOT NULL,
-  `username` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `fiscalcode` char(16) COLLATE utf8_unicode_ci NOT NULL,
-  `age` smallint(3) UNSIGNED NOT NULL,
+  `fiscalcode` char(16) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `age` smallint(3) UNSIGNED DEFAULT NULL,
   `avatar` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `roletype` enum('admin','editor','user') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Indici per le tabelle scaricate
+--
 
+--
+-- Indici per le tabelle `posts`
+--
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idx_title` (`title`),
   ADD KEY `idx_user_id` (`user_id`);
 
+--
+-- Indici per le tabelle `postscomments`
+--
 ALTER TABLE `postscomments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idx_post_id` (`post_id`),
   ADD KEY `idx_user_id` (`user_id`);
 
+--
+-- Indici per le tabelle `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `u_fiscalcode` (`fiscalcode`),
   ADD KEY `i_email` (`email`),
   ADD KEY `i_username` (`username`);
 
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
 
+--
+-- AUTO_INCREMENT per la tabella `posts`
+--
 ALTER TABLE `posts`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT per la tabella `postscomments`
+--
 ALTER TABLE `postscomments`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
 ALTER TABLE `users`
-  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
+--
+-- Limiti per le tabelle scaricate
+--
 
+--
+-- Limiti per la tabella `posts`
+--
 ALTER TABLE `posts`
   ADD CONSTRAINT `f_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Limiti per la tabella `postscomments`
+--
 ALTER TABLE `postscomments`
   ADD CONSTRAINT `fk_post_id` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
