779e8eb58372064ab5f79f112f928e5dcde416c6
hidran@gmail.com <hidran@gmail.com>
2020-04-06 00:20:01 +0200

    adds class autoloading

--- core/Router.php
+++ core/Router.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Controllers;
-
 class Router {
 
     protected $conn;
@@ -23,35 +21,35 @@ class Router {
     }
 
  public function dispatch(){
-      
+
        $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-       
+
       $url = trim($url ,'/');
-      
+
      $method = $_SERVER['REQUEST_METHOD'];
       return $this->processQueue($url, $method);
- 
-    
+
+
      }
     protected function processQueue($uri, $method = 'GET') {
           $routes = $this->routes[$method];
-  
+
         foreach ($routes as $route => $callback) {
             // converte url come '/post/:id' in regular expression
             $regMatch = preg_quote($route);
 		//	echo $regMatch."\n";
             // :id([a-zA-Z0-9\-\_]+)
-            
+
             // post/([a-zA-Z0-9\-\_]+)
            // $subPattern = preg_replace('/\\\:[a-zA-Z0-9\_\-]+/', '([a-zA-Z0-9\-\_]+)',  $regMatch);
            $subPattern = preg_replace('/\\\:[a-zA-Z0-9\_\-]+/', '([a-zA-Z0-9\-\_]+)',  $regMatch);
-            
+
             $pattern = "@^" . $subPattern. "$@D";
 			//var_dump($pattern);
              // post/4
             // @^post/([a-zA-Z0-9\-\_]+)$@;
             $matches = [];
-            
+
             //post/2
             if (preg_match($pattern, $uri, $matches)) {
                 // rimuove la prima parte trovata
@@ -59,35 +57,35 @@ class Router {
                 // 2
                 array_shift($matches);
                  return $this->route($callback, $matches);
-               
+
             }
         }
        throw new Exception('No matchin routes found for ' . $uri);
- 
-      
+
+
     }
 
           protected function route($callback, array $matches =[])
      {
          try {
-             
+
              if(is_callable($callback)){
                return   call_user_func_array($callback, $matches);
              }
-             
+
          $tokens = explode('@',$callback);
-       
+
          $controller = $tokens[0];
-         
+
          $method = $tokens[1];
-         
+
            $class =  new $controller($this->conn);
-           
+
             if(method_exists($controller, $method)){
-              
-              call_user_func_array([$class, $method], $matches); 
+
+              call_user_func_array([$class, $method], $matches);
               return $class;
-               
+
             } else {
                 throw new Exception('Method '.$method.' not found in class '. $controller);
             }
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,11 +1,25 @@
 <?php
 session_start();
-
+require_once __DIR__ . '/../helpers/functions.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-     
 
-
+function loadClass($className){
+     $filename1 = str_replace(['App','Controllers','Models','DB\\'],
+       ['app','controllers','models','db\\'], $className
+    ).'.php' ;
+    $filename2 =  str_replace('app\\','', $filename1);
+    $filename3 = "core/$className.php";
+     if(file_exists($filename1)){
+         require $filename1;
+     } elseif (file_exists($filename2)){
+         require $filename2;
+     }else {
+         require $filename3;
+     }
+}
+spl_autoload_register('loadClass');
+/*
 require_once __DIR__ . '/../db/DBPDO.php';
 
 require_once __DIR__ . '/../db/DbFactory.php';
@@ -17,6 +31,7 @@ require_once __DIR__ . '/../app/models/Post.php';
 
 require_once __DIR__ . '/../app/models/Comment.php';
 
-require_once __DIR__ . '/../helpers/functions.php';
+
 
 require_once __DIR__ . '/../core/Router.php';
+*/
